c1bed99bd655a0dab27bc57321d3518f934e8960
Greg Reda <54119966+gregreda37@users.noreply.github.com>
2020-03-11 11:13:42 -0400

    change to main activity

--- app/src/androidTest/java/com/ukrainianrestoration/officelist/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/ukrainianrestoration/officelist/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.ukrainianrestoration.officelist;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
--- app/src/main/java/com/ukrainianrestoration/officelist/MainActivity.java
+++ app/src/main/java/com/ukrainianrestoration/officelist/MainActivity.java
@@ -1,6 +1,7 @@
 package com.ukrainianrestoration.officelist;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //this was my initial commit
     }
 }
